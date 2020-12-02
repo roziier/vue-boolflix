197361a71772b36e53bb596d147acebc0e2fc88b
roziier <rikirosa9@gmail.com>
2020-12-02 17:52:42 +0100

    v-if fixed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 // BOOLFIX
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// titolo, titolo originale, lingua, voto
-
 
 var app = new Vue({
   el: '#app',
@@ -11,14 +6,17 @@ var app = new Vue({
     // userSearch will be an empty strings, we'll replace it at the end of API's call
     userSearch: '',
     // empty array that I will fill it with API's push
-    myFilms: []
+    myFilms: [],
+    myTVseries: []
   },
   methods: {
     callAPI: function () {
+      // API to get Films
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d',
       // params added to personalize the research
       {
         params: {
+          language: 'it-IT',
           query: this.userSearch
         }
       })
@@ -28,8 +26,23 @@ var app = new Vue({
           this.myFilms[i].vote_average = Math.round(this.myFilms[i].vote_average / 2);
         }
         console.log(this.myFilms);
+      }),
 
-
+      // API to get TV series
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d',
+      // params added to personalize the research
+      {
+        params: {
+          language: 'it-IT',
+          query: this.userSearch
+        }
+      })
+      .then(response => {
+        this.myTVseries = response.data.results;
+        for (var i = 0; i < this.myTVseries.length; i++) {
+          this.myTVseries[i].vote_average = Math.round(this.myTVseries[i].vote_average / 2);
+        }
+        console.log(response);
       })
     }
 
